14978258bb3c931bfafc3e38a4314d286a4ffc7e
UP220628 <up220628@alumnos.upa.edu.mx>
2026-02-17 08:26:44 -0600

    icono arreglado

--- components/forms/ReportForm.tsx
+++ components/forms/ReportForm.tsx
@@ -348,8 +348,7 @@ export const ReportForm = ({ includeProvider = false }: ReportFormProps) => {
               <svg className="w-4 h-4 sm:w-5 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
               </svg>
-              <span className="hidden xs:inline">Agregar</span>
-              <span className="xs:hidden">+</span>
+              <span className="hidden sm:inline">Agregar</span>
             </Button>
           </div>
 
