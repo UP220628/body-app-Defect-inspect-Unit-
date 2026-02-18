59cfa93ef4961f9d6023cdb38fa63d691f7a07d4
UP220628 <up220628@alumnos.upa.edu.mx>
2026-02-18 11:19:50 -0600

    poner debajo del header

--- components/ui/Dropdown.tsx
+++ components/ui/Dropdown.tsx
@@ -42,7 +42,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
   return (
     <div
       ref={dropdownRef}
-      className="fixed md:absolute md:right-0 md:top-full md:mt-2 bottom-0 md:bottom-auto left-0 md:left-auto right-0 w-full md:w-96 md:rounded-lg rounded-t-lg bg-white shadow-xl z-[9999] border border-gray-200 md:border-gray-200 max-h-[80vh] md:max-h-[80vh] flex flex-col"
+      className="fixed md:absolute md:right-0 md:top-full md:mt-2 top-24 left-0 right-0 bottom-auto md:bottom-auto w-full md:w-96 md:rounded-lg rounded-t-lg bg-white shadow-xl z-[9999] border border-gray-200 md:border-gray-200 max-h-[80vh] md:max-h-[80vh] flex flex-col"
     >
       {/* Header */}
       {title && (
